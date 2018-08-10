212142846742b47e9a8c4f06557182fe6bb195e5
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-10 13:53:47 -0700

    Refactor main.js and Add grid system

--- src/main.js
+++ src/main.js
@@ -3,52 +3,53 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Search } from './search.js';
-import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
   $(".findDoctors").submit(function(e) {
+    $(".resultsGrid").text("");
+    $(".noResult").text("");
     e.preventDefault();
     let name = $(".name").val();
     let condition = $(".condition").val();
     let AllDoctors = new Search(name, condition, "Portland");
-    let listOfDoctors = [];
-    let newDoctor;
     let promise = AllDoctors.GetDoctors();
     promise.then(function(response) {
       let body = response.data;
-      for (let x = 0; x < body.length; x++) {
-        if (body[x].practices[0].website) {
-          $(".resultsGrid").append(
-            `<div>
-              <p>${body[x].profile.first_name} ${body[x].profile.last_name}</p>
-              <p><img src='${body[x].profile.image_url}'</p>
-              <p><a href="${body[x].practices[0].website}" target="_blank">${body[x].practices[0].website}<a></p>
-              <p>${body[x].practices[0].visit_address.city}</p>
-              <p>${body[x].practices[0].visit_address.street}</p>
-              <p>${body[x].practices[0].visit_address.state}</p>
-              <p>${body[x].practices[0].visit_address.zip}</p>
-            </div>`
-          );
+      if (body.length) {
+        for (let x = 0; x < body.length; x++) {
+          if (body[x].practices[0].website) {
+            let phoneNumberArray = (body[x].practices[0].phones[0].number).split("");
+            $(".resultsGrid").append(
+              `<div class="cell">
+                <p><img src='${body[x].profile.image_url}'</p>
+                <p>${body[x].profile.first_name} ${body[x].profile.last_name}</p>
+                <p>${body[x].practices[0].visit_address.city}</p>
+                <p>${body[x].practices[0].visit_address.street}</p>
+                <p>${body[x].practices[0].visit_address.state}</p>
+                <p>${body[x].practices[0].visit_address.zip}</p>
+                <p>${phoneNumberArray[0]}${phoneNumberArray[1]}${phoneNumberArray[2]}-${phoneNumberArray[3]}${phoneNumberArray[4]}${phoneNumberArray[5]}-${phoneNumberArray[6]}${phoneNumberArray[7]}${phoneNumberArray[8]}${phoneNumberArray[9]}</p>
+                <p><a href="${body[x].practices[0].website}" target="_blank">${body[x].practices[0].website}<a></p>
+              </div>`
+            );
 
-        } else {
-          $(".resultsGrid").append(
-            `<div>
-              <p>${body[x].profile.first_name} ${body[x].profile.last_name}</p>
-              <p><img src='${body[x].profile.image_url}'</p>
-              <p>${body[x].practices[0].visit_address.city}</p>
-              <p>${body[x].practices[0].visit_address.street}</p>
-              <p>${body[x].practices[0].visit_address.state}</p>
-              <p>${body[x].practices[0].visit_address.zip}</p>
-            </div>`
-          );
+          } else {
+            let phoneNumberArray = (body[x].practices[0].phones[0].number).split("");
+            $(".resultsGrid").append(
+              `<div class="cell">
+                <p><img src='${body[x].profile.image_url}'</p>
+                <p>${body[x].profile.first_name} ${body[x].profile.last_name}</p>
+                <p>${body[x].practices[0].visit_address.city}</p>
+                <p>${body[x].practices[0].visit_address.street}</p>
+                <p>${body[x].practices[0].visit_address.state}</p>
+                <p>${body[x].practices[0].visit_address.zip}</p>
+                <p>${phoneNumberArray[0]}${phoneNumberArray[1]}${phoneNumberArray[2]}-${phoneNumberArray[3]}${phoneNumberArray[4]}${phoneNumberArray[5]}-${phoneNumberArray[6]}${phoneNumberArray[7]}${phoneNumberArray[8]}${phoneNumberArray[9]}</p>
+              </div>`
+            );
+          }
         }
-
+      } else {
+        $(".noResult").append("<p>There are no matching doctors with your input</p>");
       }
-
-      // for(let i = 0; i < body.length; i++) {
-      //   newDoctor = new Doctor(body[i].id, body[i].first_name, body[i].last_name, body[i].weight_class, body[i].thumbnail)
-      //   listOfDoctors.push(newDoctor);
-      // };
     });
   });
 
--- src/search.js
+++ src/search.js
@@ -24,22 +24,4 @@ export class Search {
       request.send();
     });
   }
-
-  // GetConditions() {
-  //   return new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     let apiKey = process.env.exports.apiKey;
-  //     let url =``;
-  //     request.responseType = 'json';
-  //     request.onload = function() {
-  //       if (this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(Error(request.statusText));
-  //       }
-  //     };
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   })
-  // }
 }
