991f065a2ec36156c0e778a65a39843ca007520a
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-10 08:49:06 -0700

    Add Doctor class

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Doctor } from "./search.js";
 
 $(document).ready(function() {
   console.log("hey, you should buy Abe a beer sometime!");
