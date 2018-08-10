be72d84ed95c08e87b06974f46cdb28539310cc0
Kevin Ahn <kevinahn7@hotmail.com>
2018-08-10 09:20:24 -0700

    Create GetAllDoctors method in Doctor class

--- src/search.js
+++ src/search.js
@@ -1,3 +1,28 @@
 export class Doctor {
-  
+  constructor(doctors) {
+    this.doctors = doctors;
+  }
+  GetAllDoctors() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let apiKey = process.env.apiKey;
+      console.log(apiKey);
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,100&skip=2&limit=10&user_key=' + apiKey;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+
+
+
+
+
 }
